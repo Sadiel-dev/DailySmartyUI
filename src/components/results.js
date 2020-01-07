87284e37e0a0b31064b0437f84ca7871cda3864f
Sadiel Almanza <almanza_sadiel@yahoo.com>
2020-01-07 09:36:33 -0500

    added files

---
+++ src/components/results.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+import SearchBar from "./searchBar";
+import ResultsPosts from './resultsPosts';
+
+import * as actions from '../actions';
+import {connect} from 'react-redux';
+
+class Results extends Component {
+
+    handleSearchBarSubmit(query){
+        this.props.fetchPostsWithQuery(query)
+
+    }
+
+    render() {
+        return (
+            <div className="results">
+                <Logo size={55}/>
+                <SearchBar page="results" onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+                <ResultsPosts/>
+            </div>
+        )
+    }
+}
+export default connect(null,actions)(Results);
